d15fc627b81db95ccd01371f582d5a898d83ebf9
Dournbrood <tanner1220002@gmail.com>
2019-11-21 14:15:30 -0500

    missing div fixed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,19 +7,20 @@ const About = () => {
   return (
     <div className="App-Wrap">
 
-<Navbar/>
-      <Card style={{backgroundColor: '#F8F5E1'}} className="shadow-sm ">
-      <h1 className="aboutTitle1">WHY</h1>
-      <h1 className="aboutTitle2">SO</h1>
-      <h1 className="aboutTitle3">SALTY?</h1>
-      <div className="saltShaker"><img src={saltShaker} alt="saltShaker" />
-      </div>
+      <Navbar />
+      <Card style={{ backgroundColor: '#F8F5E1' }} className="shadow-sm ">
+        <h1 className="aboutTitle1">WHY</h1>
+        <h1 className="aboutTitle2">SO</h1>
+        <h1 className="aboutTitle3">SALTY?</h1>
+        <div className="saltShaker"><img src={saltShaker} alt="saltShaker" />
+        </div>
       </Card>
-      <Card style={{backgroundColor: '#F8F5E1'}} className="shadow-sm d-flex justify-content-center align-items-center">
-          <div className="saltText">
-        <div className="saltPile">
-        <p className="aboutText">We make it our goal to encapsulate the essence of developer rage, the very force that was vital in the making of this website. Here, you will find scores of recorded frustration for your admiration or entertainment.</p>
-        <p className="aboutText">[more sample text]</p>
+      <Card style={{ backgroundColor: '#F8F5E1' }} className="shadow-sm d-flex justify-content-center align-items-center">
+        <div className="saltText">
+          <div className="saltPile">
+            <p className="aboutText">We make it our goal to encapsulate the essence of developer rage, the very force that was vital in the making of this website. Here, you will find scores of recorded frustration for your admiration or entertainment.</p>
+            <p className="aboutText">[more sample text]</p>
+          </div>
         </div>
       </Card>
       <Card style={{ backgroundColor: '#EDFDFA' }} className="shadow-sm d-flex justify-content-center align-items-center">
