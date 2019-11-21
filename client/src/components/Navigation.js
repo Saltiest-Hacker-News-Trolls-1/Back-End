9714b44d064d071b02f69ee0929b4105bf3f5df1
Yurimurda <lolsgotpwnd@yahoo.com>
2019-11-21 16:57:06 -0600

    Front page finished, now desktop compatible

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -8,16 +8,16 @@ const Navbar = (props) => {
 
     return (
         <Nav pills style={{ zIndex: 99 }} className="p-2 d-flex justify-content-around mb-2 shadow-sm">
-            <NavItem style={{ cursor: "pointer" }}>
+            <NavItem style={{ cursor: "pointer", background:'white', borderRadius: '5px' }}>
                 <NavLink active={pathname === "/login"} onClick={() => { props.history.push("/login") }}>Login</NavLink>
             </NavItem>
-            <NavItem style={{ cursor: "pointer" }}>
+            <NavItem style={{ cursor: "pointer", background:'white', borderRadius: '5px'  }}>
                 <NavLink active={pathname === "/register"} onClick={() => { props.history.push("/register") }}>Register</NavLink>
             </NavItem>
-            <NavItem style={{ cursor: "pointer" }}>
+            <NavItem style={{ cursor: "pointer", background:'white', borderRadius: '5px'  }}>
                 <NavLink active={pathname === "/about"} onClick={() => { props.history.push("/about") }}>About</NavLink>
             </NavItem>
-            <NavItem style={{ cursor: "pointer" }}>
+            <NavItem style={{ cursor: "pointer", background:'white', borderRadius: '5px'  }}>
                 <NavLink active={pathname === "/"} onClick={() => { props.history.push("/") }}>Home</NavLink>
             </NavItem>
         </Nav>
