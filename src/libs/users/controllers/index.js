dea332170b8f1527a4b2a117bcbfe576a837a53a
igor <igoratakhanov@gmail.com>
2019-11-20 23:00:06 -0500

    deleteAccount endpoint added, but not tested

--- src/libs/users/controllers/index.js
+++ src/libs/users/controllers/index.js
@@ -1,9 +1,7 @@
 module.exports = {
     register: require('./register'),
     login: require('./login'),
-    changePassword : require('./changePassword')
-    // auth: require('./auth')
-    // logOut: require('./logout_user'),
-    // authUser: require('./auth')
+    changePassword: require('./changePassword'),
+    deleteAccount: require('./deleteAccount')
 };
 
