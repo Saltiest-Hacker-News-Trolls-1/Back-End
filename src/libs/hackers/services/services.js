d7e244d4c005a8ae4ad1061c2cd778a4f04d3197
igor <igoratakhanov@gmail.com>
2019-11-22 02:35:02 -0500

    adjusted queries

--- src/libs/hackers/services/services.js
+++ src/libs/hackers/services/services.js
@@ -1,12 +1,12 @@
 const getTopHackers = db => async (x) => {
-    const topXHackers = await db.sequelize.query(`SELECT negativity, positivity, commentcount, id  FROM users WHERE negativity, positivity IS NOT NULL ORDER BY commentcount DESC LIMIT 10;`, {
+    const topXHackers = await db.sequelize.query(`SELECT negativity, positivity, commentcount, id  FROM users WHERE negativity IS NOT NULL ORDER BY commentcount DESC LIMIT 10;`, {
         type: db.sequelize.QueryTypes.SELECT
     })
     return topXHackers
 }
 
 const getMeanestHackers = db => async()=>{
-    const meanHackers = await db.sequelize.query(`SELECT negativity, compoundkarma, commentcount, id  FROM users WHERE negativity IS NOT NULL ORDER BY negativity DESC LIMIT 10;`, {
+    const meanHackers = await db.sequelize.query(`SELECT negativity, karma, commentcount, id  FROM users WHERE negativity IS NOT NULL ORDER BY negativity DESC LIMIT 10;`, {
         type: db.sequelize.QueryTypes.SELECT
     })
     return meanHackers
